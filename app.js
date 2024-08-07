7719714fa04ad8844134a091047c534c17f8d0e6
akriti-gupta27 <akritigupta8596@gmail.com>
2024-08-07 21:26:53 +0530

    2nd changes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 //const heading = React.createElement("h1", {id : "heading", xyz: "abc"}, "Hello World from React!");
 const parent = React.createElement("div", {id : "parent"}, 
     [React.createElement("div", {id:"child"},
-    [React.createElement("h1", {}, " I am H1 Tag"), 
+    [React.createElement("h1", {}, " I am H1 Tag 1"), 
     React.createElement("h2", {}, "I am h2 Tag")]),
     React.createElement("div", {id:"child2"},
     [React.createElement("h1", {}, " I am H1 Tag"), 
