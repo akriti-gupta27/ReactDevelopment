d5815605e69d659257747bfa45156643586bc288
akriti-gupta27 <akritigupta8596@gmail.com>
2024-08-09 15:14:11 +0530

    7th changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,43 +6,6 @@ const Body = () => {
 
     const [listofRestaurants, setListOfRestaurants] = useState(resList);
 
-    console.log(listofRestaurants);
-
-    let listofRestaurantsJS = [{
-        "info": {
-            "id": "405798",
-            "name": "Chinese Wok",
-            "cloudinaryImageId": "e0839ff574213e6f35b3899ebf1fc597",
-            "costForTwo": "₹250 for two",
-            "cuisines": [
-              "Chinese",
-              "Asian",
-              "Tibetan",
-              "Desserts"
-            ],
-            "avgRating": 4.3,
-            "sla": {
-              "deliveryTime": 50
-            }
-        }
-    },
-    {
-        "info": {
-            "id": "405788",
-            "name": "KFC",
-            "cloudinaryImageId": "e0839ff574213e6f35b3899ebf1fc597",
-            "costForTwo": "₹550 for two",
-            "cuisines": [
-              "Chinese",
-              "Desserts"
-            ],
-            "avgRating": 3.3,
-            "sla": {
-              "deliveryTime": 30
-            }
-        }
-    }];
-
     return(
         <div className="body">
             <div className="filter">
