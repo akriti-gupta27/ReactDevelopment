0de2970743f16b01cacfdbcf0d1236e9c32fd764
akriti-gupta27 <akritigupta8596@gmail.com>
2024-08-09 13:18:11 +0530

    6th changes

---
+++ src/components/Body.js
@@ -0,0 +1,26 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/mockData";
+
+const Body = () => {
+
+    const listofRestaurants = [];
+
+    return(
+        <div className="body">
+            <div className="filter">
+                <button className="filter-btn" onClick={() => {
+                    console.log("Button clicked");
+                }}>
+                    Top Rated Restaurants
+                </button>
+            </div>
+            <div className="res-container">
+                {
+                    resList.map(restaurant => <RestaurantCard key={restaurant.info.id} resData={restaurant}/>)
+                }
+            </div>
+        </div>
+    )
+};
+
+export default Body;
