c00caa12fdce073571faf582a146925bea21b00f
akriti-gupta27 <akritigupta8596@gmail.com>
2024-08-11 16:35:16 +0530

    Routing Start

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>Aboout</h1>
+            <h2>This is Akriti's Food App</h2>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,9 @@
+const Contact = () => {
+    return (
+        <div className="contact">
+            <h1>You can contact us on 1234</h1>
+        </div>
+    )
+};
+
+export default Contact;
