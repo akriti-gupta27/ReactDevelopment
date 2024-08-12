13a5aef87587c9bc7c5596ecb39fee8b720a10b2
akriti-gupta27 <akritigupta8596@gmail.com>
2024-08-12 18:22:27 +0530

    Class Based component 1

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,29 @@
-const About = () => {
-    return (
-        <div>
-            <h1>Aboout</h1>
-            <h2>This is Akriti's Food App</h2>
-        </div>
-    )
+import User from "./User";
+import UserClass from "./UserClass";
+import { Component } from "react";
+
+class About extends Component{
+    constructor(props){
+        super(props);
+        console.log("parent constrcutor");
+    }
+
+    componentDidMount(){
+        console.log("Parent did mount");
+    }
+
+
+    render(){
+        return (
+            <div>
+                <h1>About</h1>
+                <h2>This is Akriti's Food App</h2>
+                {/* <User name ={"Akriti Gupta (FUnctionalComp)"}/> */}
+                <UserClass name ={"Akriti Gupta(ClassComp)"}/>
+                <UserClass name ={"Akriti Gupta(ClassComp)"}/>
+            </div>
+        );
+    }
 }
 
 export default About;
